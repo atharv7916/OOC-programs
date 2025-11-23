f5388ee0a44e38587cb3f690cc64192f6ebc63d4
Atharv Babaso Jamanik <atharvjamnik1@gmail.com>
2025-11-23 11:27:16 +0530

    Refactor Complex class and add Student class

--- prac1.cpp
+++ prac1.cpp
@@ -1,49 +1,38 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-class Complex{
+class Student {
+private:
+	char name[30];
+	int rollNo;
+	float marks;
 public:
-int real;
-int imaginary;
-Complex ()
-{
-    real=0;
-    imaginary=0;
+// Member function to input student details
+void inputDetails() {
+	cout << "Enter student name: ";
+	cin >> name;
+	cout << "Enter roll number: ";
+	cin >> rollNo;
+	cout << "Enter marks: ";
+	cin >> marks;
 }
-    Complex (int r, int i)
-    {
-        real=r;
-        imaginary=i;
-    }
-    Complex addComplexNumber(Complex C1, Complex C2)
-    {
- Complex res;
-        res.real=C1.real+C2.real;
-        res.imaginary=C1.imaginary+C2.imaginary;
-        return res;
-}
+// Member function to display student details
+void displayDetails() {
+	cout << "Student Name: "<<name << endl;
+	cout << "Roll Number: " << rollNo << endl;
+	cout << "Marks: " << marks << endl;
+	}
 };
 int main() {
+cout<<"Name : Atharv Babaso Jamanik"<<endl;
+cout<<"Roll No.: 47"<<endl;
+cout<<"Diision : B"<<engl;
 
-    cout<<"Name: Atharv Jamanik"<<endl;
-    cout<<"Roll No: 47"<<endl;
-    cout<<" Div : CSE B"<<endl;
-    
-    Complex C1(10,4);
-    cout<<"First Complex Number: "<<C1.real<<" + i"<<C1.imaginary<<endl;
 
-    Complex C2(6,1);
-    cout<<"Second Complex Number: "<<C2.real<<" + i"<<C2.imaginary<<endl;
-    Complex C3;
-    C3=C3.addComplexNumber(C1,C2);
-    cout<<"Sum of Two Complex Number: "<<C3.real<<" + i"<<C3.imaginary<<endl;
-
-    Complex A(2,7);
-    cout<<"First Complex Number: "<<A.real<<" + i"<<A.imaginary<<endl;
-
-    Complex B(10,6);
-    cout<<"Second Complex Number: "<<B.real<<" + i"<<B.imaginary<<endl;
-    Complex D;
-    D=D.addComplexNumber(A,B);
-    cout<<"Sum of Two Complex Number: "<<D.real<<" + i"<<D.imaginary
-<<endl;
+Student studentObj;
+// Input student details
+studentObj.inputDetails();
+// Display student details
+cout << "\nStudent Details:\n";
+studentObj.displayDetails();
+return 0;
 }
